f3ca8aeeadbab91dfd7905c6b61efeaae9fb1995
Artem <105941754+ArtemJT@users.noreply.github.com>
2023-10-02 23:51:36 +0300

    Added UserControllerClassAspect

--- src/main/java/com/example/testappwithtests_springboot/util/aspect/UserControllerClassAspect.java
+++ src/main/java/com/example/testappwithtests_springboot/util/aspect/UserControllerClassAspect.java
@@ -1,4 +1,34 @@
-package com.example.testappwithtests_springboot.util.aspect;/**
+package com.example.testappwithtests_springboot.util.aspect;
+
+import com.example.testappwithtests_springboot.util.exception.DateRangeException;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+/**
  * @author Artem Kovalov on 02.10.2023
- */public record UserControllerClassAspect() {
+ */
+@Aspect
+@Component
+@Slf4j
+public record UserControllerClassAspect() {
+
+    @Pointcut("execution(public * com.example.testappwithtests_springboot.web.UserController.getAllByBirthDateRange(..))")
+    public void callAtGetAllByBirthDateRangeMethod() {
+    }
+
+    @Before("callAtGetAllByBirthDateRangeMethod()")
+    public void checkBefore(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        LocalDate from = (LocalDate) args[0];
+        LocalDate to = (LocalDate) args[1];
+        if (from.isAfter(to)) {
+            throw new DateRangeException("Date 'FROM' must be earlier than 'TO'");
+        }
+    }
 }
