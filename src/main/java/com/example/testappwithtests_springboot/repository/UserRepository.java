1a5f91da7529f40c9d6479cab05274364afc2626
Artem <105941754+ArtemJT@users.noreply.github.com>
2023-10-02 23:41:24 +0300

    Added User repository

--- src/main/java/com/example/testappwithtests_springboot/repository/UserRepository.java
+++ src/main/java/com/example/testappwithtests_springboot/repository/UserRepository.java
@@ -1,4 +1,19 @@
-package com.example.testappwithtests_springboot.repository;/**
+package com.example.testappwithtests_springboot.repository;
+
+import com.example.testappwithtests_springboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
  * @author Artem Kovalov on 30.09.2023
- */public interface UserRepository {
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findAllByBirthDateBetweenAndIsDeletedIsFalse(LocalDate from, LocalDate to);
+
+
 }
