33c2da5e328debf090d2fadbac0ba55783aed587
Artem <105941754+ArtemJT@users.noreply.github.com>
2023-10-02 23:41:06 +0300

    Added User entity class

--- src/main/java/com/example/testappwithtests_springboot/entity/User.java
+++ src/main/java/com/example/testappwithtests_springboot/entity/User.java
@@ -1,4 +1,44 @@
-package com.example.testappwithtests_springboot.entity;/**
+package com.example.testappwithtests_springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+/**
  * @author Artem Kovalov on 30.09.2023
- */public class User {
+ */
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(schema = "test_schema", name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Column
+    private String address;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    @Builder.Default
+    private Boolean isDeleted = Boolean.FALSE;
 }
