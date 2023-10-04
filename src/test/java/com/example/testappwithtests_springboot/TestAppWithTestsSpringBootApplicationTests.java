4384f4b59818d16022fd2abcf5fc8fce04b8b770
Artem <105941754+ArtemJT@users.noreply.github.com>
2023-10-05 00:35:43 +0300

    Added app context test

--- src/test/java/com/example/testappwithtests_springboot/TestAppWithTestsSpringBootApplicationTests.java
+++ src/test/java/com/example/testappwithtests_springboot/TestAppWithTestsSpringBootApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.testappwithtests_springboot;
 
+import com.example.testappwithtests_springboot.web.UserController;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class TestAppWithTestsSpringBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    @Lazy
+    private UserController userController;
+
+    @Test
+    @DisplayName("context loads")
+    void contextLoads() {
+        Assertions.assertThat(userController).isNotNull();
+    }
 
 }
